e7caff3676e3de1c5e6e0fd08c8f29d8a62e998b
GokberkUnal <58752103+GokberkUnal@users.noreply.github.com>
2021-11-14 15:26:32 +0300

    Initial Commit

---
+++ swift_sqflite/swift_sqfliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_sqfliteApp.swift
+//  swift_sqflite
+//
+//  Created by Orhan Gökberk Ünal on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_sqfliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
